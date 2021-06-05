7caa28e7bd4273a19215112b43cbd3cf30cc206a
jubair421 <32570149+jubair421@users.noreply.github.com>
2021-06-05 23:59:14 +0600

    Fetching data from db and showing in table

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 //components
 import Input from "./Components/Input";
+import Show from "./Components/Show";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
         <Input/>
+        <Show/>
       </div>
     </Fragment>
   );
